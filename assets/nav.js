523e81445273a9caa9d9d3201e5a3fc46a0ea242
doamatto <hello@doamatto.xyz>
2020-06-10 14:09:00 -0700

    Removed JS again

--- assets/nav.js
+++
@@ -1,8 +0,0 @@
-  function nav() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav") {
-      x.className += " resp";
-    } else {
-      x.className = "nav";
-    }
-  }
